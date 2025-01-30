d3511474ae5f4a2c3926db9fda963266d10c4a6c
Llouisechung <louisechung24@gmail.com>
2025-01-30 09:37:59 +0100

    new start

--- newgame.js
+++ newgame.js
@@ -1,3 +1,14 @@
-function setip() {
-    
+function setup() {
+  createCanvas(800, 800);
+  background(10, 100, 200);
+}
+
+class road {
+  constructor(x, y) {
+    this.y = y;
+    this.x = x;
+  }
+  draw() {
+    push();
+  }
 }
